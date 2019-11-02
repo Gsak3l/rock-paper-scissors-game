352260a38e19272d227e3bfa83ca4cbb1731aefa
gsak3l <gsakel5@gmail.com>
2019-11-02 18:48:39 +0200

    project almost finished

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 //document.querySelector('') can help you get the class instead of the div
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_div = document.querySelector(".result p");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
@@ -44,26 +44,28 @@ function game(userChoice) {
         case "rockrock":
         case "paperpaper":
         case "scissorsscissors":
-            draw(userChoice, computerChoice);
+            draw(userChoice);
             break;
     }
 }
 
 //functions for any possible outcome
-function win(user, computer) {
-    userScore++; 
+function win(userChoice, computerChoice) {
+    userScore++;
     userScore_span.innerText = userScore;
     computerScore_span.innerText = computerScore;
+    result_div.innerText = userChoice + " beats " + computerChoice + ". You win!🎉";
 }
 
-function lose(user, computer) {
+function lose(userChoice, computerChoice) {
     computerScore++;
     computerScore_span.innerText = computerScore;
     userScore_span.innerText = userScore;
+    result_div.innerText = computerChoice + " beats " + userChoice + ". You lose😢";
 }
 
-function draw(user, computer) {
-
+function draw(userChoice) {
+    result_div.innerText = "Tie! Both of you chose " + userChoice;
 }
 
 //computer choice function
