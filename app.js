a62999b6382c62bc42a0e9551516ab0c3b895317
gsak3l <gsakel5@gmail.com>
2019-11-02 11:38:08 +0200

    event listeners and userChoice function added

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //variables - caching the dom
 //storing something for future use
-const userScore = 0; 
+const userScore = 0;
 const computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
@@ -11,3 +11,23 @@ const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
 //document.querySelector('') can help you get the class instead of the div
 
+//main function
+function main() {
+    //event listeners for our buttons
+    rock_div.addEventListener('click', function () {
+        game(rock_div.id);
+    });
+    paper_div.addEventListener('click', function () {
+        game(paper_div.id);
+    });
+    scissors_div.addEventListener('click', function () {
+        game(scissors_div.id);
+    });
+}
+
+//game function
+function game(userChoice) {
+    console.log(userChoice);
+}
+
+main();
