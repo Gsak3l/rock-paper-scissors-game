b5d4c23b8d196d33281233d889c8421120bb65d9
gsak3l <gsakel5@gmail.com>
2019-11-02 11:51:50 +0200

    computer and user choice functions added and fully working

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const userScore = 0;
 const computerScore = 0;
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
+//document.querySelector('') can help you get the class instead of the div
 const scoreBoard_div = document.querySelector(".score-board");
 const result_div = document.querySelector(".result");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
-//document.querySelector('') can help you get the class instead of the div
+const choices = ['rock', 'paper', 'scissors'];
 
 //main function
 function main() {
@@ -27,7 +28,13 @@ function main() {
 
 //game function
 function game(userChoice) {
-    console.log(userChoice);
+    const computerChoice = getComputerChoice();
+    console.log(userChoice, computerChoice);
+}
+
+//computer choice function
+function getComputerChoice() {
+    return choices[Math.floor(Math.random() * 3)];
 }
 
 main();
